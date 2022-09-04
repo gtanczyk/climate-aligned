9a4a82c4ed65183c9f54fcd4bac68f6afaf7defa
Grzegorz Tańczyk <fb@tanczyk.pl>
2022-09-04 15:19:54 +0200

    add link to 52climateactions

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+        <p><small>Climate Aligned app is based on the content of <a href="https://www.52climateactions.com/" target="_blank">55 Climate Actions
+            initiative</a>.</small></p>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
